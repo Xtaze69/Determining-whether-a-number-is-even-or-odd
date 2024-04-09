c650399d90cc6ceb42a5d14a39228688150945a0
Xtaze <Xtaze@DESKTOP-2DTRJ02>
2024-04-10 00:45:04 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Приложение_по_определению_чётного_или_нечётного_числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long number;
+
+            Console.WriteLine("Введите число: ");
+            number = long.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
